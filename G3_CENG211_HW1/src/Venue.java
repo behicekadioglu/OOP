809426fb4e104067061ce5f69a6f90dc653608f6
Koyash <114601573+Goyaqua@users.noreply.github.com>
2024-10-09 00:45:48 +0300

    Venue Class
    Constructor and 2 new method to handle random ticket booking

--- G3_CENG211_HW1/src/Venue.java
+++ G3_CENG211_HW1/src/Venue.java
@@ -1,9 +1,44 @@
+import java.util.Random;
+
 public class Venue {
-    private int numOfSections;
+    private int numOfSections = 4;
     private Section[] sections;
 
     // Constructors
+    public Venue() {
+        sections = new Section[numOfSections];
+        for (int i = 0; i < numOfSections; i++) {
+            sections[i] = new Section(i); // Initialize each section
+        }
+    }
+    public Ticket getAvailableTicket(Section section) {
+        Ticket choosenTicket;
 
+        int attempts = 0; // Counter for attempts
+    
+        // Attempt to find an available ticket
+        while (attempts < 600) { // Try up to 600 times
+            choosenTicket = getRandomTicket(section);
+            if (!choosenTicket.isBooked()) {
+                return choosenTicket; // Return the available ticket if found
+            }
+            attempts++; // Increment attempts
+        }
+    
+        return null; // Return null if no available tickets are found after 600 attempts
+    }
+
+    // Method to get a random ticket from the venue for a specific section
+    public Ticket getRandomTicket(Section section) {
+        Random random =  new Random();
+
+        Ticket[][] ticketsArray = section.getTickets();
+        int randomRow = random.nextInt(10);
+        int randomSeat = random.nextInt(60);
+
+        Ticket choosenTicket = ticketsArray[randomRow][randomSeat];
+        return choosenTicket;
+    } 
 
     // Getters and Setters
     public int getNumOfSections() {
